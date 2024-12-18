9478521b572dd823ee516750d03adb0e3883d9c2
Nathan Griffiths <ngriffiths@postmedia.com>
2024-12-18 09:08:21 -0800

    copy edit

--- src/data/steps.js
+++ src/data/steps.js
@@ -1,6 +1,6 @@
 export default [
     'Debris flows are common in B.C.’s coastal coastal mountains because heavy rains fall on steep slopes covered in loose sediment. The Sea to Sky Highway between Horseshoe Bay and Britannia has a history of destructive debris flows. <span class="scroll-more">Scroll for more...</span>',
     'Heavy rains can swell streams along the mountain crest and cause sediment to slide into the rapid waters, forming debris flows.',
-    'The debris flows can pick up trees, sediment and speed as they pass through mountainside canyons.',
+    'The debris flows can pick up trees, sediment and speed up as they pass through mountainside canyons.',
     'As the debris flows exit the canyons, they form a fan that can damage roads, bridges and homes.'
 ];  
